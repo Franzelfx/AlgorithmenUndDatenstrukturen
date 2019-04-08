e8fd63a7cb4f50c7ab7ab94f6ce61667fe1c2b3f
Franzelfx <fabian.franz0596@gmail.com>
2019-04-08 15:02:29 +0200

    Verbesserung für negative Zahlen

--- EuklidischerAlgorithmus/main.cpp
+++ EuklidischerAlgorithmus/main.cpp
@@ -5,13 +5,13 @@
 
 #include <iostream>
 
-uint64_t ggT(uint64_t a, uint64_t b);
-uint64_t ggT_v(uint64_t a, uint64_t b);
+uint64_t ggT(int64_t a, int64_t b);
+uint64_t ggT_v(int64_t a, int64_t b);
 
 int main(){
-	uint64_t a = 100;
-	uint64_t b = 36;
-	uint64_t result = 0;
+	int64_t a = 100;
+	int64_t b = 36;
+	int64_t result = 0;
 
 	std::cout << "GGT zweier Zahlen, gib den Divisor ein: ";
 	std::cin >> a;
@@ -22,7 +22,9 @@ int main(){
 	return 0;
 }
 
-uint64_t ggT(uint64_t a, uint64_t b){
+uint64_t ggT(int64_t a, int64_t b){
+	if(a < 0) a = a * -1;
+	if(b < 0) b = b * -1;
 	if(a == 0) a = b;
 	if(a < b) {
 		uint64_t buf = a;
@@ -37,8 +39,9 @@ uint64_t ggT(uint64_t a, uint64_t b){
 	}
 	return b;
 }
-
-uint64_t ggT_v(uint64_t a, uint64_t b){
+uint64_t ggT_v(int64_t a, int64_t b){
+	if(a < 0) a = a * -1;
+	if(b < 0) b = b * -1;
 	if(a == 0) a = b;
 	if(a < b) {
 		uint64_t buf = a;
